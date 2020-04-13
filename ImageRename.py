c6a4f0019caa372ba1aa6bdcd237981a8f280125
Ritik Agarwal <ritik2209@gmail.com>
2020-04-13 23:38:32 +0530

    Create ImageRename.py

---
+++ ImageRename.py
@@ -0,0 +1,212 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 26 20:35:19 2019
+
+@author: USER
+"""
+
+import os
+'''
+i=0
+path="C:/Users/USER/Desktop/GAIP Project/Project Images/angry boy/"
+for filename in  os.listdir(path):
+      my_dest ="angry" +str(i) +"."+filename[-3:] 
+      my_source =path + filename
+      my_dest =path + my_dest
+      # rename() function will
+      # rename all the files
+      os.rename(my_source, my_dest)
+      i += 1
+'''
+
+'''
+i=485
+path="C:/Users/USER/Desktop/GAIP Project/Project Images/angry girl/"
+for filename in  os.listdir(path):
+      my_dest ="angry" +str(i) +"."+filename[-3:] 
+      my_source =path + filename
+      my_dest =path + my_dest
+      # rename() function will
+      # rename all the files
+      os.rename(my_source, my_dest)
+      i += 1      
+'''
+
+'''
+i=0
+path="C:/Users/USER/Desktop/GAIP Project/Project Images/sad boy/"
+for filename in  os.listdir(path):
+      my_dest ="sad" +str(i) +"."+filename[-3:] 
+      my_source =path + filename
+      my_dest =path + my_dest
+      # rename() function will
+      # rename all the files
+      os.rename(my_source, my_dest)
+      i += 1
+'''
+
+'''
+i=599
+path="C:/Users/USER/Desktop/GAIP Project/Project Images/sad girl/"
+for filename in  os.listdir(path):
+      my_dest ="sad" +str(i) +"."+filename[-3:] 
+      my_source =path + filename
+      my_dest =path + my_dest
+      # rename() function will
+      # rename all the files
+      os.rename(my_source, my_dest)
+      i += 1
+'''
+'''
+i=1253
+path="C:/Users/USER/Desktop/GAIP Project/Project Images/sad man/"
+for filename in  os.listdir(path):
+      my_dest ="sad" +str(i) +"."+filename[-3:] 
+      my_source =path + filename
+      my_dest =path + my_dest
+      # rename() function will
+      # rename all the files
+      os.rename(my_source, my_dest)
+      i += 1   
+'''
+
+'''
+i=1809
+path="C:/Users/USER/Desktop/GAIP Project/Project Images/sad woman/"
+for filename in  os.listdir(path):
+      my_dest ="sad" +str(i) +"."+filename[-3:] 
+      my_source =path + filename
+      my_dest =path + my_dest
+      # rename() function will
+      # rename all the files
+      os.rename(my_source, my_dest)
+      i += 1
+'''
+
+'''
+i=832
+path="C:/Users/USER/Desktop/GAIP Project/Project Images/angry man/"
+for filename in  os.listdir(path):
+      my_dest ="angry" +str(i) +"."+filename[-3:] 
+      my_source =path + filename
+      my_dest =path + my_dest
+      # rename() function will
+      # rename all the files
+      os.rename(my_source, my_dest)
+      i += 1
+'''
+
+'''
+i=1741
+path="C://Users//USER//Desktop//GAIP Project//Project Images//angry woman2//"
+for filename in  os.listdir(path):
+      my_dest ="angry" +str(i) +"."+filename[-3:] 
+      my_source =path + filename
+      my_dest =path + my_dest
+      # rename() function will
+      # rename all the files
+      os.rename(my_source, my_dest)
+      i += 1
+'''
+
+'''
+i=0
+path="C:/Users/USER/Desktop/GAIP Project/Project Images/disgusted boy/"
+for filename in  os.listdir(path):
+      my_dest ="disgusted" +str(i) +"."+filename[-3:] 
+      my_source =path + filename
+      my_dest =path + my_dest
+      # rename() function will
+      # rename all the files
+      os.rename(my_source, my_dest)
+      i += 1
+'''
+
+'''
+i=740
+path="C:/Users/USER/Desktop/GAIP Project/Project Images/disgusted girl2/"
+for filename in  os.listdir(path):
+      my_dest ="disgusted" +str(i) +"."+filename[-3:] 
+      my_source =path + filename
+      my_dest =path + my_dest
+      # rename() function will
+      # rename all the files
+      os.rename(my_source, my_dest)
+      i += 1
+'''
+
+'''
+i=771
+path="C:/Users/USER/Desktop/GAIP Project/Project Images/disgusted man/"
+for filename in  os.listdir(path):
+      my_dest ="disgusted" +str(i) +"."+filename[-3:] 
+      my_source =path + filename
+      my_dest =path + my_dest
+      # rename() function will
+      # rename all the files
+      os.rename(my_source, my_dest)
+      i += 1
+'''
+   
+'''  
+i=1084
+path="C:/Users/USER/Desktop/GAIP Project/Project Images/disgusted woman2/"
+for filename in  os.listdir(path):
+      my_dest ="disgusted" +str(i) +"."+filename[-3:] 
+      my_source =path + filename
+      my_dest =path + my_dest
+      # rename() function will
+      # rename all the files
+      os.rename(my_source, my_dest)
+      i += 1
+'''
+
+
+i=0
+path="C:/Users/USER/Desktop/GAIP Project/Project Images/happy boy/"
+for filename in  os.listdir(path):
+      my_dest ="happy" +str(i) +"."+filename[-3:] 
+      my_source =path + filename
+      my_dest =path + my_dest
+      # rename() function will
+      # rename all the files
+      os.rename(my_source, my_dest)
+      i += 1
+
+
+'''
+i=339
+path="C:/Users/USER/Desktop/GAIP Project/Project Images/happy girl/"
+for filename in  os.listdir(path):
+      my_dest ="happy" +str(i) +"."+filename[-3:] 
+      my_source =path + filename
+      my_dest =path + my_dest
+      # rename() function will
+      # rename all the files
+      os.rename(my_source, my_dest)
+      i += 1
+'''
+
+'''
+i=547
+path="C:/Users/USER/Desktop/GAIP Project/Project Images/happy man/"
+for filename in  os.listdir(path):
+      my_dest ="happy" +str(i) +"."+filename[-3:] 
+      my_source =path + filename
+      my_dest =path + my_dest
+      # rename() function will
+      # rename all the files
+      os.rename(my_source, my_dest)
+      i += 1
+'''
+
+i=785
+path="C:/Users/USER/Desktop/GAIP Project/Project Images/happy woman/"
+for filename in  os.listdir(path):
+      my_dest ="happy" +str(i) +"."+filename[-3:] 
+      my_source =path + filename
+      my_dest =path + my_dest
+      # rename() function will
+      # rename all the files
+      os.rename(my_source, my_dest)
+      i += 1
